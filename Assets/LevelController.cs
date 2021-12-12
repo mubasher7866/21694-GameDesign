2f0193d6cc64efc91d713180bdeaf4d89b9023ff
Mubasher <21694@student.dorset-college.ie>
2021-12-12 13:04:48 +0000

    first

---
+++ Assets/LevelController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+        private static int _nextLevelIndex = 1;
+        private Enemy[] _enemies;
+
+        private void OnEnable()
+        {
+            _enemies = FindObjectsOfType<Enemy>();
+            
+        }
+
+        void Update()
+    {
+
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("You Killed all enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+        
+    }
+}
